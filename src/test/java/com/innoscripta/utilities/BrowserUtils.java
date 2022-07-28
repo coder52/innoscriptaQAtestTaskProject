3195b5e1ef0720be69b0e3cc2683ed712d8be3dd
Muhsin Sahin <sahinmuhsin2018@gmail.com>
2022-07-28 23:49:06 +0200

    Add clickElementInFrame method

--- src/test/java/com/innoscripta/utilities/BrowserUtils.java
+++ src/test/java/com/innoscripta/utilities/BrowserUtils.java
@@ -8,14 +8,12 @@ public class BrowserUtils {
     /**
      * waits for backgrounds processes on the browser to complete
      *
-     * @param timeOutInSeconds
+     * @param timeOutInSeconds maximum time
      */
     public static void waitForPageToLoad(long timeOutInSeconds) {
-        ExpectedCondition<Boolean> expectation = new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver driver) {
-                return ((JavascriptExecutor) driver).executeScript("return document.readyState").equals("complete");
-            }
-        };
+        ExpectedCondition<Boolean> expectation = driver -> ((JavascriptExecutor) driver)
+                .executeScript("return document.readyState")
+                .equals("complete");
         try {
             WebDriverWait wait = new WebDriverWait(Driver.getDriver(), timeOutInSeconds);
             wait.until(expectation);
@@ -34,14 +32,28 @@ public class BrowserUtils {
     /**
      * Performs a pause
      *
-     * @param seconds
+     * @param seconds standby time
      */
     public static void waitFor(int seconds) {
         try {
-            Thread.sleep(seconds * 1000);
+            Thread.sleep(seconds * 1000L);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+    /**
+     *  This method switches the frame from default to another one inside the default frame.
+     *
+     * @param elementCss css selector of a target frame in the current frame.
+     * @param frameCss css selector of an element in the target frame.
+     */
+    public static void clickElementInFrame(By frameCss, By elementCss) {
+        // switch in to frame
+        Driver.getDriver().switchTo().frame(Driver.getDriver().findElement(frameCss));
+        // click on element in frame
+        Driver.getDriver().findElement(elementCss).click();
+        // switch back to default content
+        Driver.getDriver().switchTo().defaultContent();
     }
 }
 
