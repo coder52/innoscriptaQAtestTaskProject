004dbafe94dca03878e1f883cf0a80b6666f5bdf
Muhsin Sahin <sahinmuhsin2018@gmail.com>
2022-07-28 23:49:55 +0200

    update InnoscriptaMainPage

--- src/test/java/com/innoscripta/pages/InnoscriptaMainPage.java
+++ src/test/java/com/innoscripta/pages/InnoscriptaMainPage.java
@@ -1,7 +1,10 @@
 package com.innoscripta.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
 
 public class InnoscriptaMainPage extends BasePage{
 
@@ -34,4 +37,37 @@ public class InnoscriptaMainPage extends BasePage{
 
     @FindBy(xpath = "//a[. = 'Willkommen']")
     public WebElement willkommenMenuItem;
+
+    public By cookiesAblehnenButton = By.cssSelector("div #onetrust-reject-all-handler");
+    public By appointmentFrame = By.cssSelector("div.calendly-popup iframe");
+
+    @FindBy(css = "td button[type='button']")
+    public List<WebElement> daysInCalendar;
+
+    @FindBy(css = ".calendly-popup-content iframe")
+    public WebElement calendarFrame;
+
+    @FindBy(css = "button div[data-component*='today-dot']")
+    public WebElement todayInCalendar;
+
+    @FindBy(css = "button[data-container='time-button']")
+    public WebElement timeButtonInCalendar;
+
+    @FindBy(xpath = "(//*[. = 'Bestätigen'])[1]")
+    public WebElement timeAcceptationButton;
+
+    @FindBy(id = "full_name_input")
+    public WebElement nameInput;
+
+    @FindBy(id = "email_input")
+    public WebElement emailInput;
+
+    @FindBy(css = "input[name=\"question_0\"]")
+    public WebElement firmaNameInput;
+
+    @FindBy(css = "textarea[name=\"question_1\"]")
+    public WebElement infoInput;
+
+    @FindBy(xpath = "(//*[. = 'Termin buchen'])[1]")
+    public WebElement terminBuchenButton;
 }
